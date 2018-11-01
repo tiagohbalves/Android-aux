eaab966791ec82c7b9f1771c72e42a85ca4fce7e
tiagohbalves <tiagohbalves@hotmail.com>
2018-11-01 12:00:18 -0300

    Splash Screen and Navigation screen

---
+++ Views/app/src/main/java/com/example/tiagohbalves/coins/SplashScreen.java
@@ -0,0 +1,31 @@
+package com.example.tiagohbalves.coins;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT = 2500;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            /*
+             * Exibindo splash com um timer.
+             */
+            @Override
+            public void run() {
+                // Esse método será executado sempre que o timer acabar
+                // E inicia a activity principal
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+
+                // Fecha esta activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
